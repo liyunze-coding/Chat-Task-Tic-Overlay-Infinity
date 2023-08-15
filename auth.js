6689a1d66c23fd1a345f469c2e80e4e807092536
liyunze <50455574+liyunze-coding@users.noreply.github.com>
2023-08-15 16:22:45 +0800

    typo

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ const auth = (function () {
 	// Authentication and channels - required
 	const channel = ""; // your channel
 	const username = ""; // bot account
-	const oauth = ""; // should be oauth:xxxxxxxxxxxx from the bot account
+	const oauth = ""; // should be oauth:xxxxxxxxxxxx from the bot account DO NOT INCLUDE 'oauth:'
 
 	return {
 		channel,
