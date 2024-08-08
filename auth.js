4927cccc8208607d3d3696f28ae6a675a4fe3aef
Cecilia <S535795@nwmissouri.edu>
2024-08-07 23:06:18 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 const auth = (function () {
 	// Authentication and channels - required
-	const channel = ""; // your channel
-	const username = ""; // bot account or your channel
+	const channel = "CeciliaRoselle"; // your channel
+	const username = "CeciliaRoselle"; // bot account or your channel
 
 	// You may or may not include the "oauth:" portion of your token
-	const oauth = "";
+	const oauth = "gjne9boojm0541ckct0jdi2mc3hyhf";
 
 	return {
 		channel,
